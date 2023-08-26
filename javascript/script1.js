be5f4921584c62d5f6effe02c483f75347219919
fernyfigs <figueroafernando1985@gmail.com>
2023-08-26 15:12:56 -0300

    commit

---
+++ javascript/script1.js
@@ -0,0 +1,8 @@
+// Primer ejercicio
+
+// Creá variables nombre y apellido y asignarles valores de tipo string
+var nombre = "Fox";
+var apellido = "Mulder";
+
+// Como segundo paso, hace un console.log() que dentro tenga la suma de las dos variables.
+console.log(nombre + " " + apellido);
---
+++ javascript/script2.js
@@ -0,0 +1,16 @@
+// Segundo ejercicio
+
+// Como segundo ejercicio, creá dos variables numéricas y luego creá una tercera variable que se llame suma y dentro de ésta última, suma las primeras dos. Hacé un console.log de suma
+
+var numero1 = 100;
+var numero2 = 200;
+var suma = numero1 + numero2;
+
+console.log(suma);
+
+//Luego, creá una variable numérica más y que su contenido sea una suma entre la variable suma y un número. Hacé otro console.log de esa variable
+
+var numero3 = 300;
+var nuevaSuma = suma + numero3;
+
+console.log(nuevaSuma);
